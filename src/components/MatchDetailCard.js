e84c918793b56fc69a1a425fcfcf56b58054ea9a
ramji013 <ramprm@gmail.com>
2021-04-26 06:29:34 +0800

    initial check-in for dashboard

---
+++ src/components/MatchDetailCard.js
@@ -0,0 +1,9 @@
+import {React} from 'react'
+
+export const MatchDetailCard = () => {
+    return (
+        <div className="TeamPage">
+            <h3> Latest Matches</h3>
+        </div>
+    )
+};
