11a7b121b82128d3c70c76c61ca39fe92f4bdb81
ramji013 <ramprm@gmail.com>
2021-04-28 22:00:03 +0800

    useEffect, useState hook added

--- src/pages/TeamPage.js
+++ src/pages/TeamPage.js
@@ -1,11 +1,25 @@
-import {React} from 'react';
+import {React, useEffect, useState} from 'react';
 import {MatchDetailCard} from '../components/MatchDetailCard';
 import { MatchSmallCard } from '../components/MatchSmallCard';
 
 export const TeamPage = () => {
+
+    const [team, setTeam] = useState(() => '');
+
+    useEffect(() => {
+        const fetchMatches = async() => {
+            const response = await fetch('http://localhost:8080/team/Delhi%20Capitals');
+            const data = await response.json();
+          
+            setTeam(data);
+        }; 
+        fetchMatches();
+    });
+    
     return (
+    
         <div className="TeamPage">
-            <h1> Team Name</h1>
+            <h1> {team.teamName}</h1>
             <MatchDetailCard/>
             <MatchSmallCard/>
             <MatchSmallCard/>
