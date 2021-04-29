ab1b81365a28c265e0b7bb3e02488ab32940b172
ramji013 <ramprm@gmail.com>
2021-04-30 05:33:54 +0800

    commiting matches pages

--- src/pages/MatchPage.js
+++ src/pages/MatchPage.js
@@ -1,10 +1,30 @@
-import {React} from 'react';
+import {React, useEffect, useState} from 'react';
+import {MatchDetailCard} from '../components/MatchDetailCard';
+import { useParams } from 'react-router-dom';
 
 export const MatchPage = () => {
 
+    const[matches, setMatches] = useState([]);
+    const {teamName, year} = useParams();
+
+    useEffect(() => {
+        const fetchMatches = async () => {
+          const response = await fetch(`http://localhost:8080/team/${teamName}/matches?year=${year}`);
+          const data = await response.json();
+          setMatches(data);
+        };  
+        fetchMatches();
+      }, []);
+
+      
     return (
         <div className="MatchPage">
-            <h1> match page</h1>
+        <h1>Match Page</h1>
+        {
+            matches.map((match) => (
+            <MatchDetailCard match={match} teamName={teamName} />
+          ))
+        }
         </div>
     )
 };
