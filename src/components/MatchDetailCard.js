07e82a3e8384a8f7f96426a24c99c1c97dac4cca
ramji013 <ramprm@gmail.com>
2021-04-30 16:40:31 +0800

    styling added for the ui

--- src/components/MatchDetailCard.js
+++ src/components/MatchDetailCard.js
@@ -1,22 +1,35 @@
 import { React } from "react";
 import { useRouteMatch, Link } from "react-router-dom";
+import "../css/MatchDetailCard.scss";
 
 export const MatchDetailCard = ({ match, teamName }) => {
   if (!match) return null;
   const otherTeam = match.team1 === teamName ? match.team2 : match.team1;
   const otherTeamRoute = `/teams/${otherTeam}`;
+  const isMatchWon = teamName === match.matchWinner;
   return (
-    <div className="TeamPage">
-      <h3> Latest Matches</h3>
-      <h1>
-        {" "}
-        vs <Link to={otherTeamRoute}>{otherTeam}</Link>
-      </h1>
-      <h2>{match.date}</h2>
-      <h3>at {match.venue}</h3>
-      <h3>
-        {match.matchWinner} won by {match.resultMargin} {match.result}
-      </h3>
+    <div className={isMatchWon ? 'MatchDetailCard won-card' : 'MatchDetailCard lost-card'}>
+      <div>
+        <span className="vs"> vs </span>
+        <h1>
+          <Link to={otherTeamRoute}>{otherTeam}</Link>
+        </h1>
+        <h2 className="match-date">{match.date}</h2>
+        <h3 className="match-venue">at {match.venue}</h3>
+        <h3 className="match-result">
+          {match.matchWinner} won by {match.resultMargin} {match.result}
+        </h3>
+      </div>
+      <div className="additional-detail">
+        <h3>First Innings</h3>
+        <p>{match.team1}</p>
+        <h3>Second Innings</h3>
+        <p>{match.team2}</p>
+        <h3>Man of the Match</h3>
+        <p>{match.playOfMatch}</p>
+        <h3>Umpires</h3>
+        <p>{match.umpire1}, {match.umpire2}</p>
+      </div>
     </div>
   );
 };
--- src/pages/TeamPage.js
+++ src/pages/TeamPage.js
@@ -2,6 +2,9 @@ import { React, useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { MatchDetailCard } from "../components/MatchDetailCard";
 import { MatchSmallCard } from "../components/MatchSmallCard";
+import "../css/TeamPage.scss";
+import { PieChart } from 'react-minimal-pie-chart';
+
 
 export const TeamPage = () => {
   const [team, setTeam] = useState({ matches: [] });
@@ -21,11 +24,29 @@ export const TeamPage = () => {
   }
   return (
     <div className="TeamPage">
-      <h1> {team.teamName}</h1>
-      <MatchDetailCard teamName={team.teamName} match={team.matches[0]} />
+      <div className="team-name-section">
+        <h1 className="team-name"> {team.teamName}</h1>
+      </div>
+      <div className="win-loss">
+          Win / Losses
+
+          <PieChart
+  data={[
+    { title: 'Wins', value: team.totalWins, color: '#4da375' },
+    { title: 'Losses', value: team.totalMatches - team.totalWins, color: '#e15454' }
+  ]}
+/>
+      </div>
+      <div className="match-detail">
+      <h3> Latest Matches</h3>
+        <MatchDetailCard teamName={team.teamName} match={team.matches[0]} />
+      </div>
       {team.matches.slice(1).map((match) => (
         <MatchSmallCard match={match} teamName={team.teamName} />
       ))}
+      <div className="more-link">
+        <a href="#">More ></a>
+      </div>
     </div>
   );
 };
