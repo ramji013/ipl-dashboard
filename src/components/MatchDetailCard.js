085e76281a61c4a01edd2aaa7f9d81290fe7200e
ramji013 <ramprm@gmail.com>
2021-05-01 11:17:52 +0800

    removed unused imports

--- src/components/MatchDetailCard.js
+++ src/components/MatchDetailCard.js
@@ -1,5 +1,5 @@
 import { React } from "react";
-import { useRouteMatch, Link } from "react-router-dom";
+import { Link } from "react-router-dom";
 import "../css/MatchDetailCard.scss";
 
 export const MatchDetailCard = ({ match, teamName }) => {
